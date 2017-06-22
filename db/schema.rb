37cf2127fcc53ae8a3469bbd4186e274979c8280
Kheira Benmeridja <kheira.benmeridja@gmail.com>
2017-06-22 19:00:36 +0200

    scrapping de m

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170622123941) do
+ActiveRecord::Schema.define(version: 20170622150024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 20170622123941) do
     t.float "average_rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "city"
+    t.string "phone"
   end
 
   create_table "reviews", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# puts 'On détruit la base de données'
+# Restaurant.destroy_all
+# Review.destroy_all
+# Booking.destroy_all
+require 'open-uri'
+
+puts 'On récupere les restaurants sur fooding'
+
+store = []
+for i in (0...5) do
+html_doc = Nokogiri::HTML(open("https://lefooding.com/fr/restaurants?filters%5Bgeo%5D%5Bcity%5D%5B%5D=526ba3f07f55b7f39a007e28&facets%5Bessentials%5D%5B%5D=526dac727f55b742d5002051&page=#{i}"), nil, 'UTF-8')
+p html_doc.search('.result-infos')
+html_doc.search('.results .results-list .results-item .result-infos .result-info.address').each do |info|
+  restaurant = {}
+  restaurant[:name] = info.search('.result-title a').first.text
+  restaurant[:address] = info.search('.street-name').text
+  restaurant[:city] = info.search('.city').text
+  restaurant[:phone] = info.search('.phone').text
+  restaurant[:chef] = "Franck B"
+  restaurant[:rating] = rand(6)
+  store << restaurant
+  end
+end
+
+  store.each do |restaurant|
+  Restaurant.create(restaurant)
+end
+puts "#{store.length} restaurant(s) "
+
